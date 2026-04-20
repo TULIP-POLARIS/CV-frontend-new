6c5a3ba05f46365e6930d272850b1ec4b2d6c91a
Ifeoluwaexam2025 <iogungbe24@students.oamk.fi.com>
2026-04-20 12:12:19 +0300

    Fix fonts for web

--- App.tsx
+++ App.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './navigation/StackNavigator';
 import { AuthProvider } from './context/AuthContext';
+import {useFonts} from 'expo-font';
+import { Ionicons, MaterialIcons, FontAwesome } from '@expo/vector-icons';
+
 
 export default function App() {
+  const [fontsLoaded]= useFonts({
+    ...Ionicons.font,
+    ...MaterialIcons.font,
+    ...FontAwesome.font
+  });
+  if(!fontsLoaded){
+    return null;
+  }
+
   return (
     <AuthProvider>
       <NavigationContainer>
