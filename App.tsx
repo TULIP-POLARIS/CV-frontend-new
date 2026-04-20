0c5d48e7d335f0ae518132ea51d44c2d389ad625
Ifeoluwaexam2025 <iogungbe24@students.oamk.fi.com>
2026-04-20 14:44:18 +0300

    lets see

--- App.tsx
+++ App.tsx
@@ -3,12 +3,27 @@ import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './navigation/StackNavigator';
 import { AuthProvider } from './context/AuthContext';
 import { useFonts } from 'expo-font';
-import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import {
+  Ionicons,
+  MaterialIcons,
+  FontAwesome,
+  FontAwesome5,
+  MaterialCommunityIcons,
+  AntDesign,
+  Entypo,
+  Feather,
+} from '@expo/vector-icons';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     ...Ionicons.font,
     ...MaterialIcons.font,
+    ...FontAwesome.font,
+    ...FontAwesome5.font,
+    ...MaterialCommunityIcons.font,
+    ...AntDesign.font,
+    ...Entypo.font,
+    ...Feather.font,
   });
 
   if (!fontsLoaded) {
