742ed58f29122973f66961cd2238d108d53da6c6
Ifeoluwaexam2025 <iogungbe24@students.oamk.fi.com>
2026-04-21 12:06:03 +0300

    Resolved Merge Conflicts

--- components/cv/CVTemplatePreview.tsx
+++ components/cv/CVTemplatePreview.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
 import HamburgerMenu from "../../components/HamburgerMenu";
-import
+
 
 export default function CVTemplatePreview() {
   return (
